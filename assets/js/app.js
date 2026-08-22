540084b1f9db277fc4cbe022a64b27752897e55d
Manuel Valenzuela <manu.valenzuelac@duocuc.cl>
2026-08-22 13:19:01 -0400

    chore: actualiza repo

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const botonBienvenida = document.querySelector("#boton-bienvenida");
+
+if (botonBienvenida) {
+  botonBienvenida.addEventListener("click", () => {
+    alert("Conecta Cultura se construye paso a paso.");
+  });
+}
